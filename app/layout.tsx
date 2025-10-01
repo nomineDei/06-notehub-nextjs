bddc38d0b559bbd0c181fd7bb32b7a404318aa99
Roman <motkoroma2702@gmail.com>
2025-10-01 17:42:23 +0200

    corrected tanstackprivider 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
-import TanStackProvider from "@/components/TanStackProvider/TanstackProvider";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
